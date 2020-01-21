bf65390ff299a2788d1d00f80c8f1fd7dd584f1f
Jerry Brown <jbbrown@wpi.edu>
2020-01-21 08:14:31 -0500

    Some more work on 6-1

--- src/days/six.rs
+++ src/days/six.rs
@@ -1,6 +1,8 @@
 use crate::days::Day;
 use crate::Result;
 use crate::StringError;
+use std::collections::HashMap;
+use std::rc::Rc;
 
 pub struct Six {
     filename: &'static str
@@ -16,20 +18,43 @@ impl Day for Six {
   fn run(&self) -> Result<String> {
     let contents = crate::util::read_file(self.filename)?;
     let lines = contents.lines();
-    
-    for line in lines {
-        let orbitPair: Vec<&str> = line.split(")").collect();
-        let oribed = match orbitPair.get(0) {
-            Some(v) => *v,
-            None => return Err(Box::new(StringError::new("Orbit pair does not contain orbited.".to_string())))
-        };
 
-        let orbiter = match orbitPair.get(1) {
-            Some(v) => *v,
-            None => return Err(Box::new(StringError::new("Orbit pair does not contain orbiter.".to_string())))
-        };
+    let mut graph: HashMap<String, Node> = HashMap::new();
+    
+    // build tree
+    for line in lines {
+      let orbitPair: Vec<&str> = line.split(")").collect();
+      let orbited = match orbitPair.get(0) {
+          Some(v) => *v,
+          None => return Err(Box::new(StringError::new("Orbit pair does not contain orbited.".to_string())))
+      }.to_string();
+
+      let orbiter = match orbitPair.get(1) {
+          Some(v) => *v,
+          None => return Err(Box::new(StringError::new("Orbit pair does not contain orbiter.".to_string())))
+      }.to_string();
+
+      if let Some(node) = graph.get_mut(&orbited) {
+        node.edges.push(orbiter.clone());
+      } else {
+        let mut node = Node { value: orbited.clone(), edges: Vec::new() };
+        node.edges.insert(0, orbiter.clone());
+        graph.insert(orbited, node);
+      }
+
+      if !graph.contains_key(&orbiter) {
+        let node = Node { value: orbiter.clone(), edges: Vec::new() };
+        graph.insert(orbiter, node);
+      }
     }
 
     Ok(String::from("123"))
   }
+}
+
+//----
+
+struct Node {
+  value: String,
+  edges: Vec<String>
 }
