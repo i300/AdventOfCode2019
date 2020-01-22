23f6fecf287d3d0c3a4fdb8020b00f046ed84148
Jerry Brown <jbbrown@wpi.edu>
2020-01-21 23:58:19 -0500

    6-2 done

--- src/days/six.rs
+++ src/days/six.rs
@@ -2,7 +2,6 @@ use crate::days::Day;
 use crate::Result;
 use crate::StringError;
 use std::collections::HashMap;
-use std::rc::Rc;
 
 pub struct Six {
     filename: &'static str
@@ -21,40 +20,89 @@ impl Day for Six {
 
     let mut graph: HashMap<String, Node> = HashMap::new();
     
-    // build tree
+    // build graph
     for line in lines {
-      let orbitPair: Vec<&str> = line.split(")").collect();
-      let orbited = match orbitPair.get(0) {
+      let orbit_pair: Vec<&str> = line.split(")").collect();
+      
+      let parent = match orbit_pair.get(0) {
           Some(v) => *v,
           None => return Err(Box::new(StringError::new("Orbit pair does not contain orbited.".to_string())))
       }.to_string();
 
-      let orbiter = match orbitPair.get(1) {
+      let child = match orbit_pair.get(1) {
           Some(v) => *v,
           None => return Err(Box::new(StringError::new("Orbit pair does not contain orbiter.".to_string())))
       }.to_string();
 
-      if let Some(node) = graph.get_mut(&orbited) {
-        node.edges.push(orbiter.clone());
+      if let Some(node) = graph.get_mut(&child) {
+        node.parent = parent.clone();
       } else {
-        let mut node = Node { value: orbited.clone(), edges: Vec::new() };
-        node.edges.insert(0, orbiter.clone());
-        graph.insert(orbited, node);
-      }
+        let node = Node { parent: parent.clone(), children: Vec::new() };
+        graph.insert(child.clone(), node);
+      } 
 
-      if !graph.contains_key(&orbiter) {
-        let node = Node { value: orbiter.clone(), edges: Vec::new() };
-        graph.insert(orbiter, node);
+      if let Some(node) = graph.get_mut(&parent) {
+        node.children.push(child.clone());
+      } else {
+        let mut node = Node { parent: String::new(), children: Vec::new() };
+        node.children.push(child.clone());
+        graph.insert(parent.clone(), node);
       }
     }
 
-    Ok(String::from("123"))
+    // Part 1
+    let mut sum_distances = 0;
+    for (key, _) in &graph {
+      sum_distances += find_com(&graph, key)?;
+    }
+
+    // Part 2
+    let you = "YOU".to_string();
+    let san_distance = find_san(&graph, &you, &you)? - 3; // Minus 1 for SAN, 1 for YOU, and 1 for the node SAN orbits
+    Ok(format!("Sum distances = {}, # of transfers to SAN = {}", sum_distances.to_string(), san_distance.to_string()))
+  }
+}
+
+fn find_com(graph: &HashMap<String, Node>, start: &String) -> Result<i32> {
+  // If we are COM, then the distance is 0
+  if start == "COM" {
+    return Ok(0)
+  }
+  // Otherwise recurse down the tree
+  if let Some(node) = graph.get(start) {
+    Ok(1 + find_com(graph, &node.parent)?)
+  } else {
+    return Err(Box::new(StringError::new(format!("Node {} not found in graph.", start))))
+  }
+}
+
+fn find_san(graph: &HashMap<String, Node>, start: &String, from: &String) -> Result<i32> {
+  if start == "SAN" {
+    return Ok(1)
+  }
+  if let Some(node) = graph.get(start) {
+    let mut search_set: Vec<String> = node.children.to_vec();
+    search_set.push(node.parent.clone());
+
+    for node in search_set.iter() {
+      if node == from || node.is_empty() {
+        continue
+      } 
+      let result = find_san(&graph, node, start)?;
+      if result >= 1 {
+        return Ok(result + 1)
+      }
+    }
+
+    Ok(0)
+  } else {
+    return Err(Box::new(StringError::new(format!("Node {} not found in graph.", start))))
   }
 }
 
 //----
 
 struct Node {
-  value: String,
-  edges: Vec<String>
+  parent: String,
+  children: Vec<String>
 }
